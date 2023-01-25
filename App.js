4ce5573aa65c99f072ab494e8e72fc12ade9f27a
neuboi <38407565+neuboi@users.noreply.github.com>
2023-01-24 21:33:52 -0500

    Expo sensors (Accelerometer) Test
    * Testing expo-sensors implementation

--- App.js
+++ App.js
@@ -1,7 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Accelerometer } from 'expo-sensors';
 
 export default function App() {
+
+  const [{ x, y, z }, setData] = useState({
+  x: 0,
+  y: 0,
+  z: 0,
+  });
+  const [subscription, setSubscription] = useState(null);
+
+  const _slow = () => Accelerometer.setUpdateInterval(1000);
+  const _fast = () => Accelerometer.setUpdateInterval(16);
+
+  const _subscribe = () => {
+    setSubscription(
+      Accelerometer.addListener(setData)
+    );
+  };
+
+  const _unsubscribe = () => {
+    subscription && subscription.remove();
+    setSubscription(null);
+  };
+
+  useEffect(() => {
+    _subscribe();
+    return () => _unsubscribe();
+  }, []);
+
+
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
